7f57bec0ef11578469524ff82ff195da833f01dd
Gilvan André Gobbato <gilvangobbato@gmail.com>
2022-01-15 09:46:06 -0300

    Initial commit

---
+++ src/main/java/com/github/gilvangobbato/SbootAccountTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.gilvangobbato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootAccountTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootAccountTransactionsApplication.class, args);
+	}
+
+}
