b596c9b0a7dc4cadd3c5d43d94cd4339ca511e54
Gilvan André Gobbato <gilvangobbato@gmail.com>
2022-01-17 08:19:26 -0300

    Adicionando comentáarios

--- src/main/java/com/github/gilvangobbato/util/StringUtils.java
+++ src/main/java/com/github/gilvangobbato/util/StringUtils.java
@@ -2,6 +2,12 @@ package com.github.gilvangobbato.util;
 
 public class StringUtils {
 
+    /**
+     * Verify if de string is only numbers
+     *
+     * @param value
+     * @return
+     */
     public static boolean isOnlyNumbers(String value) {
         return value.matches("^[0-9]*$");
     }
